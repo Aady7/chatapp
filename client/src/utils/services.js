b32ebb6120116f8f5e96c6f46faa5bff0cb08206
Aady <pratapadityasingh2703@gmail.com>
2024-05-27 19:38:32 +0530

    chore: Fix typo in getRequest function

--- client/src/utils/services.js
+++ client/src/utils/services.js
@@ -1,26 +1,35 @@
-export const baseUrl = 'http://localhost:3000/api';
+export const baseUrl = "http://localhost:3000/api";
 export const postRequest = async (url, body) => {
-    const response=await fetch(`${baseUrl}/${url}`, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body,
-    });
-    const data=await response.json();
-    if(!response.ok){
-        let message
-        if(data?.message){
-            message=data.message
-        }
-        else{
-            message=data;
-
-        }
-        return {error:true, message}
+  const response = await fetch(`${baseUrl}/${url}`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body,
+  });
+  const data = await response.json();
+  if (!response.ok) {
+    let message;
+    if (data?.message) {
+      message = data.message;
+    } else {
+      message = data;
     }
-        
-   
-    
-    return data;
+    return { error: true, message };
+  }
+
+  return data;
+};
+
+export const getRequest = async (url) => {
+  const response = await fetch(`${baaseUrl}/${url}`);
+  const data = await response.json();
+  if (!response.ok) {
+    let message = "Something went wrong";
+    if (data?.message) {
+      message = data.message;
+    }
+    return { error: true, message };
+  }
+  return data;
 };
